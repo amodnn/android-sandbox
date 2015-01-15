f93886a07ad30929e5d0e4257270bf3c0473e99f
kyokomi <kyoko1220adword@gmail.com>
2015-01-15 20:29:04 +0900

    evernoteのlibraryが古かったのを更新

--- SwipeRefreshListExample/library/src/main/java/com/evernote/client/android/AsyncReflector.java
+++ SwipeRefreshListExample/library/src/main/java/com/evernote/client/android/AsyncReflector.java
@@ -4,6 +4,8 @@ import android.os.Handler;
 import android.os.Looper;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -11,6 +13,25 @@ import java.util.concurrent.Executors;
  * Class that uses reflection to asynchronously wrap Client methods
  */
 class AsyncReflector {
+
+  /**
+   * List of primitives to convert from autoboxed method calls
+   */
+  public final static Map<Class<?>, Class<?>> PRIMITIVE_MAP = new HashMap<Class<?>, Class<?>>();
+  static {
+    PRIMITIVE_MAP.put(Boolean.class, boolean.class);
+    PRIMITIVE_MAP.put(Byte.class, byte.class);
+    PRIMITIVE_MAP.put(Short.class, short.class);
+    PRIMITIVE_MAP.put(Character.class, char.class);
+    PRIMITIVE_MAP.put(Integer.class, int.class);
+    PRIMITIVE_MAP.put(Long.class, long.class);
+    PRIMITIVE_MAP.put(Float.class, float.class);
+    PRIMITIVE_MAP.put(Double.class, double.class);
+  }
+
+  /**
+   * Singled threaded Executor for async work
+   */
   private static ExecutorService sThreadExecutor = Executors.newSingleThreadExecutor();
   /**
    * Reflection to run Asynchronous methods
@@ -22,13 +43,20 @@ class AsyncReflector {
         try {
           Class[] classes = new Class[args.length];
           for (int i = 0; i < args.length; i++) {
-            classes[i] = args[i].getClass();
+            //Convert Autoboxed primitives to actual primitives (ex: Integer.class to int.class)
+            if(PRIMITIVE_MAP.containsKey(args[i].getClass())) {
+              classes[i] = PRIMITIVE_MAP.get(args[i].getClass());
+            } else {
+              classes[i] = args[i].getClass();
+            }
           }
 
           Method method = null;
           if(receiver instanceof Class) {
+            //Can receive a class if using for static methods
             method = ((Class)receiver).getMethod(function, classes);
           } else {
+            //used for instance methods
             method = receiver.getClass().getMethod(function, classes);
           }
 
